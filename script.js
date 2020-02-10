39eb81a4d2adc02d65ad0c420de8819d9e2b7471
James Geib <jpgeib2@gmail.com>
2020-02-10 12:49:43 -0800

    clock function finalized

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ function clock() {
     var hour = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
+    var midday = "AM";
+    midday = (hour >= 12) ? "PM" : "AM";
+    hour = (hour == 0 ) ? 12 : ((hour > 12) ? (hour - 12): hour);
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
 
-    document.getElementById("currentTime").innerText = hour + " : " + min + " : " + sec;
+    document.getElementById("currentTime").innerText = hour + " : " + min + " : " + sec + " " + midday;
     var t = setTimeout(function() {
         clock();
     }, 1000);
+    
+    
 };
 
 //Time update function
