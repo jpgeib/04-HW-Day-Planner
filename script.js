60b02d43ebcf05cf32a03c7a259f0ea82c6be0de
James Geib <jpgeib2@gmail.com>
2020-02-10 14:00:10 -0800

    preparing ids and DOM elements for timeslots

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-//Clock function
+//Timeslot DOMs
+const nineAM = document.getElementById("timeslot-9AM");
+const tenAM = document.getElementById("timeslot-10AM");
+const elevenAM = document.getElementById("timeslot-11AM");
+const twelvePM = document.getElementById("timeslot-12PM");
+const onePM = document.getElementById("timeslot-1PM");
+const twoPM = document.getElementById("timeslot-2PM");
+const threePM = document.getElementById("timeslot-3PM");
+const fourPM = document.getElementById("timeslot-4PM");
+const fivePM = document.getElementById("timeslot-5PM");
 
+//Clock function
 
 function clock() {
     var time = new Date();
